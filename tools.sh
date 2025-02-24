279ff9435aa404b1c125f4041153212b3fc42051
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2025-02-24 18:07:11 +0530

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# For Ubuntu 22.04
+sudo apt update -y
+
+# Installing Docker
+#!/bin/bash
+sudo apt update
+sudo apt install docker.io -y
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo chmod 777 /var/run/docker.sock
+
+# Installing AWS CLI
+#!/bin/bash
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Installing Terraform
+#!/bin/bash
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform -y
